e7d6d6aca45a8f71267a7e443544999f7f770064
suraj_barailee <baraileesuraj@gmail.com>
2022-12-16 17:27:49 +0545

    ref and reactive

--- vue_with_server/composition_api/src/App.vue
+++ vue_with_server/composition_api/src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <section class="container">
     <h2>{{ userName }}</h2>
+    <h3> {{ age }}</h3>
+    <h3> {{ userObject }}</h3>
+    <h3> {{ userObject2 }}</h3>
   </section>
 </template>
 
 <script>
+import { ref,reactive } from 'vue';
 export default {
-  data() {
-    return {
-      userName: 'Maximilian',
-    };
+  setup() {
+    const userName = ref('Michael Jackson');
+    const age = ref(50);
+    const userObject = ref({ name: 'Michael Jackson', age: 50 });
+    const userObject2 = reactive({ name: 'Michael Jackson ReactiveNess', age: 50 });
+    setTimeout(() => {
+      userName.value = 'Michael Jackson 2';
+      userObject.value.name = 'Michael Jackson 2';
+      userObject2.name = 'Michael Jackson 2 ReactiveNess 123123';
+    }, 3000);
+    return { userName,age,userObject,userObject2 };
   },
 };
 </script>
